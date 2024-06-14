7fee00441730f299a40c3ed52638045740b88972
ellldar <eldarnyc@gmail.com>
2024-06-14 19:52:35 +0600

    updating header

--- client/components/header.js
+++ client/components/header.js
@@ -19,7 +19,7 @@ export default ({ currentUser }) => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<Link className="navbar-brand" href="/">GitTix</Link>
+			<Link className="navbar-brand" href="/">Fabao Kyrgyzstan</Link>
 			<div className="d-flex justify-content-end">
 				<ul className="nav d-flex align-items-center">
 					{links}
